43194bcc1aa6efd4279bf8855ed9968cf99afa65
nahidrasel <nhdrasel@gmail.com>
2022-05-15 23:10:10 +1200

    Second Commit

--- CSSeleniumFramework/E2ETests.cs
+++ CSSeleniumFramework/E2ETests.cs
@@ -1,16 +1,45 @@
 using CSSeleniumFramework.utilities;
 using NUnit.Framework;
+using System;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System.Linq;
+using AngleSharp.Text;
+using System.Collections.Generic;
 
 namespace CSSeleniumFramework
 {
     public class E2ETests : Base
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void EndToEndFlow()
         {
+            String[] expectedProducts = { "iphone X", "Blackberry" };
+            String[] actualProducts = new string[2];
+
+            driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
+            driver.FindElement(By.Name("Password")).SendKeys("learning");
+            driver.FindElement(By.XPath("//div[@class='form-group'][5]/label/span/input")).Click();
+            driver.FindElement(By.XPath("//input[@value='Sign in']")).Click();
+            
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(driver.Value.FindElement(By.Id("signInBtn")), "Sign In"));
+
+            IList<WebElement> products = driver.FindElements(By.TagName("app-card"));
+            foreach (IWebElement product in products)
+            {
+                if (expectedProducts.Contains(product.FindElement(By.CssSelector(".card-footer button"))
+                {
+                    product.FindElement(By.CssSelector(".card-footer button")).Click();
+                }
+                TestContext.Progress.WriteLine(product.FindElement(By.CssSelector(".card-footer button"))
+            }
         }
 
-        [Test]
+    }
+
+    [Test]
         public void Test1()
         {
             Assert.Pass();
--- CSSeleniumFramework/utilities/Base.cs
+++ CSSeleniumFramework/utilities/Base.cs
@@ -1,20 +1,25 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 using System;
-
+using System.Configuration;
 using WebDriverManager.DriverConfigs.Impl;
 
 namespace CSSeleniumFramework.utilities
 {
     public  class Base
     {
-       public IWebDriver driver;
+      
+        public IWebDriver driver;
         [SetUp]
         public void StartBrowser()
         {
-            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-            driver = new ChromeDriver();
+            //configuration
+            String browserName = ConfigurationManager.AppSettings["browser"];
+
+            InitBrowser("browserName");
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
@@ -22,6 +27,32 @@ namespace CSSeleniumFramework.utilities
 
             driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
 
+        }
+        public void InitBrowser(string browserName)
+        {
+            switch (browserName)
+            {
+                case "Firefox":
+                    new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "Chrome":
+                    new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                    driver = new ChromeDriver();
+                    break;
+
+                case "Edge":
+                    new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+                    driver = new EdgeDriver();
+                    break;
+            }
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
         }
 
     }
