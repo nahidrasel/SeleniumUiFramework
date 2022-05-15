92d86d9b62daff8aaac47fc304c86e666f7385e5
nahidrasel <nhdrasel@gmail.com>
2022-05-15 20:42:11 +1200

    Initial Commit

---
+++ CSSeleniumFramework/E2ETests.cs
@@ -0,0 +1,19 @@
+using CSSeleniumFramework.utilities;
+using NUnit.Framework;
+
+namespace CSSeleniumFramework
+{
+    public class E2ETests : Base
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CSSeleniumFramework/utilities/Base.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CSSeleniumFramework.utilities
+{
+    public  class Base
+    {
+       public IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+
+        }
+
+    }
+}
